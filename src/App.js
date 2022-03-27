5b5356eb3daea43a7eb0917dd36db923b3da2795
Madhusudhan Reddy <madhuman1012@gmail.com>
2022-03-27 18:14:59 +0530

    small tweaks

--- src/App.js
+++ src/App.js
@@ -2,17 +2,12 @@ import "./App.css";
 
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
-import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
-import { useState } from "react";
 import { FeedbackProvider } from "./context/FeedbackContext";
-import AboutPage from "./pages/AboutPage";
 import Footer from "./components/Footer";
 
 function App() {
-    const [feedback, setFeedback] = useState(FeedbackData);
-
     return (
         <>
             <FeedbackProvider>
